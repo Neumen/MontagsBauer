3a5d3e72d6eef2ac00a240c4122a15a602cd56da
Kevin <kevin.n2610@gmail.com>
2023-01-07 13:47:42 +0100

    added exceptions

--- src/main/java/de/neumen/minecraft/Commands/CommandAddPlayerMontagsBauerGame.java
+++ src/main/java/de/neumen/minecraft/Commands/CommandAddPlayerMontagsBauerGame.java
@@ -1,9 +1,11 @@
 package de.neumen.minecraft.Commands;
 
+import de.neumen.minecraft.Gamemode.MontagsBauerGame;
 import de.neumen.minecraft.Gamemode.MontagsBauerGamesManager;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandAddPlayerMontagsBauerGame extends de.neumen.minecraft.Commands.CustomCommand {
     public CommandAddPlayerMontagsBauerGame() {
@@ -16,7 +18,21 @@ public class CommandAddPlayerMontagsBauerGame extends de.neumen.minecraft.Comman
             if (strings[2] == null) {
                 throw new Exception("No player was provided.");
             }
-            MontagsBauerGamesManager.getInstance().getGame(strings[1]).addPlayer(Bukkit.getPlayer(strings[2]));
+
+            Player player = Bukkit.getPlayer(strings[2]);
+
+            if (player == null) {
+                throw new Exception("Player may not be null.");
+            }
+
+            for (MontagsBauerGame game:MontagsBauerGamesManager.getInstance().getGames()) {
+                if (game.getPlayers().contains(player)) {
+                    throw new Exception(player.getName() + " " + "is already part of: " + game.getGameTitle());
+                }
+            }
+
+            MontagsBauerGamesManager.getInstance().getGame(strings[1]).addPlayer(player);
+
             commandSender.sendMessage("Added player" + " " + strings[2] + " to game" + " " + strings[1]);
         } catch (Exception e) {
             commandSender.sendMessage(e.getMessage());
